e80af46c58d3cd241bcb756bcf17839a8d94ef89
buzzcz <buzz3333@gmail.com>
2015-11-21 15:33:10 +0100

    Changed doc style

--- Server/src/server.c
+++ Server/src/server.c
@@ -11,7 +11,9 @@
 /*
  * Creates new server socket and sets server attributes
  *
+ *
  * server_socket: server socket to be created
+ *
  *
  * return: server attributes
  * */
@@ -24,6 +26,7 @@ struct sockaddr_in create_server_socket(int *server_socket) {
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//	server_addr.sin_addr.s_addr = htons(INADDR_ANY);
 	server_addr.sin_port = htons(10000);
 
 	return server_addr;
@@ -32,8 +35,11 @@ struct sockaddr_in create_server_socket(int *server_socket) {
 /*
  * Binds server to host and port from server attributes
  *
+ *
  * server_socket: server socket to be bound
+ *
  * server_addr: server attributes for the socket to be bound to
+ *
  * server_addr_length: length of the server attributes
  * */
 void bind_server_socket(int server_socket, struct sockaddr_in server_addr, socklen_t server_addr_length) {
@@ -48,6 +54,7 @@ void bind_server_socket(int server_socket, struct sockaddr_in server_addr, sockl
 /*
  * Method that runs the server's endless loop
  *
+ * 
  * server_socket: socket to be used for receiving and sending messages
  * */
 void run_server(int server_socket) {
@@ -62,8 +69,8 @@ void run_server(int server_socket) {
 
 		printf("Server is waiting for data\n");
 		received = receive_message(server_socket, &client_addr, &client_addr_length);
-		printf("Client sent: %s", received.data);
-		respond(server_socket, client_addr, client_addr_length, &sent_datagrams, received);
+		printf("Client sent: %s\n", received.data);
+		respond(server_socket, client_addr, client_addr_length, received, &games);
 
 //		TODO remove
 		if (sent_datagrams < 0) {
--- Server/src/server.h
+++ Server/src/server.h
@@ -2,9 +2,7 @@
 #define SERVER
 
 struct sockaddr_in create_server_socket(int *server_socket);
-
 void bind_server_socket(int server_socket, struct sockaddr_in server_addr, socklen_t server_addr_length);
-
 void run_server(int server_socket);
 
 #endif
