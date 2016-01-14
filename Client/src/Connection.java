3b6e2ae3dd1b922763e4248f898e72f7c5848c1c
buzzcz <buzz3333@gmail.com>
2016-01-14 19:25:51 +0100

    Receive message checks format of message

--- Client/src/Connection.java
+++ Client/src/Connection.java
@@ -49,11 +49,12 @@ public class Connection {
 		} catch (SocketException e) {
 			System.out.println("Socket could not be made");
 		}
-		try {
+//		TODO timeout
+		/*try {
 			socket.setSoTimeout(3000);
 		} catch (SocketException e) {
 			System.out.println("Socket timeout could not be set");
-		}
+		}*/
 	}
 
 	/**
@@ -71,17 +72,12 @@ public class Connection {
 	public void sendMessage(Message message) {
 		DatagramPacket send = new DatagramPacket(message.getMessageByte(), message.getMessageByte().length, host,
 				port);
+		System.out.println("Client is sending: " + message.getMessage());
 		try {
 			socket.send(send);
-			numberOfSentDatagrams++;
 		} catch (IOException e) {
 			System.out.println("Message could not be sent");
 		}
-	}
-
-	private int[] peekMessage() {
-//		TODO peek message and return bytes to read and message size
-		return null;
 	}
 
 	/**
@@ -90,26 +86,38 @@ public class Connection {
 	 * @return received message
 	 */
 	public Message receiveMessage() throws IOException {
-//		TODO buffer size??
-		byte[] buffer = new byte[256];
+		byte[] buffer = new byte[1024];
 		DatagramPacket receive = new DatagramPacket(buffer, buffer.length);
 		socket.receive(receive);
-		numberOfReceivedDatagrams++;
 
 		String s = new String(buffer);
-		int number, type, checksum, dataSize;
+		System.out.println("Server sent: " + s);
+		int index, number, type, checksum, dataSize;
 		String data;
 
-		number = Integer.parseInt(s.substring(0, s.indexOf(';')));
-		s = s.substring(s.indexOf(';') + 1);
-		type = Integer.parseInt(s.substring(0, s.indexOf(';')));
-		s = s.substring(s.indexOf(';') + 1);
-		checksum = Integer.parseInt(s.substring(0, s.indexOf(';')));
-		s = s.substring(s.indexOf(';') + 1);
-		dataSize = Integer.parseInt(s.substring(0, s.indexOf(';')));
-		data = s;
+		try {
+			index = s.indexOf(';');
+			if (index == -1) throw new NumberFormatException("Number not found");
+			number = Integer.parseInt(s.substring(0, index));
+			s = s.substring(index + 1);
+			index = s.indexOf(';');
+			if (index == -1) throw new NumberFormatException("Type not found");
+			type = Integer.parseInt(s.substring(0, index));
+			s = s.substring(index + 1);
+			index = s.indexOf(';');
+			if (index == -1) throw new NumberFormatException("Checksum not found");
+			checksum = Integer.parseInt(s.substring(0, index));
+			s = s.substring(index + 1);
+			index = s.indexOf(';');
+			if (index == -1) throw new NumberFormatException("Data size not found");
+			dataSize = Integer.parseInt(s.substring(0, index));
+			s = s.substring(index + 1);
+			data = s;
 
-		return new Message(number, type, checksum, dataSize, data);
+			return new Message(number, type, checksum, dataSize, data);
+		} catch (NumberFormatException e) {
+			return new Message(-1, -1, -1, "");
+		}
 	}
 
 	/**
@@ -122,11 +130,31 @@ public class Connection {
 	}
 
 	/**
+	 * Returns number of sent datagrams and increments it
+	 *
+	 * @return number of sent datagrams
+	 */
+	public int increaseNumberOfSentDatagrams() {
+		return numberOfSentDatagrams++;
+	}
+
+	/**
 	 * Getter for number of sent datagrams
 	 *
 	 * @return number of received datagrams
 	 */
 	public int getNumberOfReceivedDatagrams() {
 		return numberOfReceivedDatagrams;
+	}
+
+	/**
+	 * Increments number of received datagrams
+	 */
+	public void increaseNumberOfReceivedDatagrams() {
+		numberOfReceivedDatagrams++;
+	}
+
+	public void setSocketTimeout(int timeout) throws SocketException {
+		socket.setSoTimeout(timeout);
 	}
 }
